a87ba160e469c33e9e048c452b34d220c58de781
Gorilla Facts <inquiry.gorillafacts@gmail.com>
2025-09-05 09:09:05 +0000

    अभी भी अरर या रहा है यार क्या हमे firebase मे कुछ ओर भी करना है |
    Downl

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap" rel="stylesheet" />
         <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@700&display=swap" rel="stylesheet" />
-        <link rel="manifest" href="/manifest.json" />
+        
         <meta name="theme-color" content="#D81B60" />
       </head>
       <body className="font-body antialiased">
